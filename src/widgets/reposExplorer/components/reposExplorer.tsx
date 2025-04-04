16537cc3a03d78745169ec4fcd0f061da0697e14
Denis Shadrin <deshaser@gmail.com>
2025-04-04 14:22:29 +0300

    feature/infinite-scroll: added common useDebounce

--- src/widgets/reposExplorer/components/reposExplorer.tsx
+++ src/widgets/reposExplorer/components/reposExplorer.tsx
@@ -1,27 +1,28 @@
 'use client';
+
+import { useState } from 'react';
+
 import { RepoCard } from '@/entities/repositories';
 import { useGetRepositories } from '@/entities/repositories/hooks/useGetRepositories';
 import { Input } from '@/shared/components/ui/Input';
 import { InfiniteScroll } from '@/shared/components/ui/InfiniteScroll/InfiniteScroll';
-import { useState } from 'react';
+import { useDebounce } from '@/shared/lib/hooks/useDebounce';
 
 export const ReposExplorer = () => {
   const [login, setLogin] = useState('');
-  const { repos, isLoading, hasMore, loadMore } = useGetRepositories(login);
+  const debouncedLogin = useDebounce(login, 300);
+  const { repos, isLoading, hasMore, loadMore } = useGetRepositories(debouncedLogin);
 
   return (
     <div className="flex flex-col gap-8 w-full max-w-prose">
       <Input
-        {...{
-          name: 'login',
-          label: 'Логин GitHub',
-          placeholder: 'Введите логин для поиска репозиториев',
-          value: login,
-          onChange: e => {
-            setLogin(e.target.value);
-          },
-        }}
+        name="login"
+        label="Логин GitHub"
+        placeholder="Введите логин для поиска репозиториев"
+        value={login}
+        onChange={e => setLogin(e.target.value)}
       />
+
       <InfiniteScroll
         onLoadMore={loadMore}
         isLoading={isLoading}
@@ -33,14 +34,18 @@ export const ReposExplorer = () => {
             repo={repo}
           />
         ))}
+
         {!hasMore && repos && repos.length > 0 && (
           <p className="text-center text-gray-500 my-4">
             Больше репозиториев не найдено
           </p>
         )}
       </InfiniteScroll>
-      {!!login && !isLoading && !repos?.length && (
-        <p className="text-center text-gray-500">Репозитории не найдены</p>
+
+      {!!debouncedLogin && !isLoading && !repos?.length && (
+        <p className="text-center text-gray-500">
+          Репозитории не найдены
+        </p>
       )}
     </div>
   );
